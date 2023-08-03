1520be465c23e7b7799013e3323ea55e5aceaeb9
aceaxe16 <branimir88@abv.bg>
2023-08-03 22:36:01 +0300

    added interface User

---
+++ tattoo-shops-project/src/app/types/user.ts
@@ -0,0 +1,15 @@
+export interface User{
+    "lastLogin": number | null,
+    "userStatus": string,
+    "created": number,
+    "accountType": string,
+    "ownerId": string,
+    "socialAccount": string,
+    "oAuthIdentities": JSON | null,
+    "___class": string,
+    "blUserLocale": string,
+    "updated": number,
+    "objectId": string,
+    "email": string,
+    "username": string
+}
