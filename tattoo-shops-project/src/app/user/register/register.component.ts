d76477f64d5cb925e08e6343a3aff591707cba6e
aceaxe16 <branimir88@abv.bg>
2023-08-05 19:30:53 +0300

    reactive register form that posts user in db

--- tattoo-shops-project/src/app/user/register/register.component.ts
+++ tattoo-shops-project/src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor(private userService: UserService, private router: Router){}
 
+  register(form: NgForm):void{
+    if(form.invalid){
+      return
+    }
+
+   const {username, email, password} = form.value
+   this.userService.register(username, email, password).subscribe(() =>{
+    this.router.navigate(['/shops'])
+   })
+   
+    
+  }
 }
--- tattoo-shops-project/src/app/user/user.service.ts
+++ tattoo-shops-project/src/app/user/user.service.ts
@@ -1,10 +1,44 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnDestroy } from '@angular/core';
+import { User } from '../types/user';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+import { environment } from '../environment/entironment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService implements OnDestroy{
+  private user$$ = new BehaviorSubject<User | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
+  user: User | undefined;
+
+  subscription: Subscription;
   
 
-  constructor() { }
+
+  constructor(private http: HttpClient) {
+    this.subscription = this.user$.subscribe((user) => {
+      this.user = user;
+    })
+   }
+   
+
+  register(
+    username: string, 
+    email: string, 
+    password: string, 
+    )
+    { 
+      const {appUrl} = environment;
+      return this.http.post<User>(`${appUrl}/Users`, {
+        username,
+        email,
+        password        
+      }).pipe(tap((user) => this.user$$.next(user)));
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
