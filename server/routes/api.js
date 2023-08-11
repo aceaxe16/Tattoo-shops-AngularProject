c645e094c7e17684a07e6fb4aa77c4bb63155b43
aceaxe16 <branimir88@abv.bg>
2023-08-11 23:46:34 +0300

    post request on /register functions + atlas is connected

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://branimir88:vgxRxOYslloFK25F@cluster0.98gga50.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(db);
+
+
 router.get('/', (req, res) => {
-    res.send('From API route')
+    res.send(!!mongoose.connect(db) + "  Hello from api route")
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save().then((result) => {
+        res.status(200).send(result)
+    }).catch((err) => {
+        console.log(err);
+    })
 })
 
 module.exports = router
